9236df4105035fcf8acb6497b3700278132c914a
Ivan Marakhovsky <33633636+notnotzero@users.noreply.github.com>
2021-12-18 11:20:12 -0600

    Add files via upload

---
+++ Assignment4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "mpi.h"
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[]) {
+	MPI_Init(&argc, &argv);
+	int rank, n, i, message;
+	MPI_Status status;
+	MPI_Comm_size(MPI_COMM_WORLD, &n);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 9)
+	{
+		cout << "Hello from process " << rank << "\n";
+		for (i = 1; i < n; i++) {
+			MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE,
+			         MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			cout << "Hello from process " << double(message) / 9 << endl;
+
+		}
+	}
+	else MPI_Send(&rank, 1, MPI_INT, 9, 0, MPI_COMM_WORLD);
+	MPI_Finalize();
+	return 0;
+}
